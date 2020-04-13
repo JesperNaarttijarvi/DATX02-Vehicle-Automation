31d3d95bbe8593c3ef2b39171acb6dbb37a1ab51
adamND <adam.davidsson@hotmail.se>
2020-04-13 11:20:39 +0200

    add file as input for simulation

--- Riskestimator/mybot_ws/spawnbots.py
+++ Riskestimator/mybot_ws/spawnbots.py
@@ -9,23 +9,44 @@ from geometry_msgs.msg import *
 
 #from beginner_tutorials.srv import *
 
+class simInterface : 
+    def __init__(self):
+         self.numRobots = 0
 
+    def spawn_bot(self,name,point = Point(0,0,0),quaternion = Quaternion(0,0,0,0)) :
+        rospy.wait_for_service("/gazebo/spawn_urdf_model")
 
-def spawn_bot(name,x = 0,y = 0,z = 1) :
-    rospy.wait_for_service("/gazebo/spawn_urdf_model")
+        namespace = "/robot" + str(self.numRobots)
+        self.numRobots += 1
+        
+        try:
+            spawner = rospy.ServiceProxy("/gazebo/spawn_urdf_model", SpawnModel)
+            spawner(str(name), open("/home/adam/Documents/gitHub/DATX02-Vehicle-Automation/Riskestimator/mybot_ws/src/mybot_description/urdf/mybot.urdf",'r').read(), namespace, Pose(position= point,orientation=quaternion),"world")
+        except rospy.ServiceException as e:
+            print("Service call failed: ",e)
 
-    try:
-        spawner = rospy.ServiceProxy("/gazebo/spawn_urdf_model", SpawnModel)
-        spawner(str(name), open("/home/adam/Documents/gitHub/DATX02-Vehicle-Automation/Riskestimator/mybot_ws/src/mybot_description/urdf/mybot.urdf",'r').read(), "/rover", Pose(position= Point(int(x),int(y),int(z)),orientation=Quaternion(0,0,0,0)),"world")
-    except rospy.ServiceException as e:
-        print("Service call failed: ",e)
+    def parse(self,line) : 
+        words  = line.split(" ")
+        print("1")
+        if  words[0] == "spawn"  :    
+            print("spawning bot")
+            point = Point(float(words[2]),float(words[3]),float(words[4]))
+            if len(words) == 5 :
+                self.spawn_bot(words[1],point)
+            elif len(words) == 9 : 
+                quaternion = Quaternion(float(words[5]),float(words[6]),float(words[7]),float(words[8]))
+                self.spawn_bot(words[1],point,quaternion)
+            
+        
 
 if __name__ == "__main__":
+    sI  = simInterface()
+    if len(sys.argv) == 2: 
+        f = open(sys.argv[1], "r")
+        for line in f:
+            sI.parse(line)        
     while(1) : 
-        inputs = str(raw_input(">"))
-        iargs  = inputs.split(" ")
-        
-        if  iargs[0] == "spawn" and len(iargs) == 5 :    
-            spawn_bot(iargs[1],iargs[2],iargs[3],iargs[4])
+        line = str(raw_input(">"))
+        sI.parse(line)
 
 
