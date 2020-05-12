672155f6bd4990620154b0da8e4a4858f65d3836
Marcus Svensson <msvenss@student.chalmers.se>
2020-05-12 16:21:05 +0200

    Added deviation calc

--- privacyReporting/testScript.py
+++ privacyReporting/testScript.py
@@ -1,10 +1,28 @@
 import main
+import math
 loop_range = int(input("how many runs should the test perform?"))
 
 count = 0
 subtotal = 0
+numbers = []
+n = 0
 for i in range(loop_range):
-    subtotal = subtotal + main.test()
+    n = main.test()
+
+    numbers.append(n)
+    subtotal = subtotal + n
     count = count + 1
 
-print("average value reported is: ", (subtotal / count))
+mean = (subtotal / count)
+
+subtotal_squared = 0
+for i in range(len(numbers)):
+    numbers[i] = pow(numbers[i] - mean, 2)
+    subtotal_squared = subtotal_squared + numbers[i]
+
+squared_mean = (subtotal_squared / count)
+deviation = pow(squared_mean, (1/2.0))
+
+print("average value reported is: ", mean)
+print("deviation: ", deviation)
+
